b893cf98f140b078d093c6286eb56126c56a5c92
Jas3dwards <jasonedw@mtu.edu>
2025-10-21 16:43:06 -0400

    Add files via upload

--- Assets/Scripts/CameraControler.cs
+++ Assets/Scripts/CameraControler.cs
@@ -5,7 +5,7 @@ public class CameraControl  : MonoBehaviour{
 
     private void Update(){
  
-        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);    
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);    
     }
 
 }
