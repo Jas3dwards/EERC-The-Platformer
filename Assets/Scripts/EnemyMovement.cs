7b9441c138a0daaa75546e20d92081876b56f113
seonwoo03 <seonwook@mtu.edu>
2025-10-28 15:51:09 -0400

    Update enemy and shooting system

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,12 +2,27 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+    private void Start()
+    {
+        Debug.Log("EnemyAttack script active!");
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Triggered with: " + collision.name); 
+        Debug.Log("Triggered with: " + collision.name + " / Tag: " + collision.tag);
+
         if (collision.CompareTag("Player"))
         {
-            collision.GetComponent<PlayerHealth>().TakeDamage(1);
+            PlayerHealth health = collision.GetComponent<PlayerHealth>();
+            if (health != null)
+            {
+                Debug.Log("Dealing 1 damage to player");
+                health.TakeDamage(1);
+            }
+            else
+            {
+                Debug.LogWarning("No PlayerHealth component found on " + collision.name);
+            }
         }
     }
 }
