a58b1f0763e391a0e5d47daf8cd710985d59a922
seonwoo03 <seonwook@mtu.edu>
2025-11-17 01:17:05 -0500

    Added traps, fireball ranged attack, map expansion and bug fixes

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerHealth player = collision.GetComponent<PlayerHealth>();
+            if (player != null)
+            {
+                player.TakeDamage(damage);
+            }
+        }
+    }
+}
